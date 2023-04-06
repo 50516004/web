e621a3a8e1b26a97ec21a8935b420197dda466fb
50516004 <iwakura.kenshin.15@shizuoka.ac.jp>
2023-04-07 07:14:13 +0900

    編集

--- edit_table/main.js
+++ edit_table/main.js
@@ -1,55 +1,66 @@
-function createCalendar(year, month) {
-    // カレンダーを表示する要素を取得
-    var calendar = document.getElementById("calendar");
+// Get table and button elements
+const table = document.getElementById("myTable");
+const addButton = document.getElementById("add-row-button");
 
-    // 月の最初の日を取得
-    var firstDay = new Date(year, month-1, 1);
+// Add event listener for add row button
+addButton.addEventListener("click", addRow);
 
-    // 月の最後の日を取得
-    var lastDay = new Date(year, month, 0);
+// Add event listener for edit and delete buttons in the table
+table.addEventListener("click", function(event) {
+  if (event.target.classList.contains("edit-button")) {
+    editRow(event.target.parentNode.parentNode);
+  }
+  if (event.target.classList.contains("delete-button")) {
+    deleteRow(event.target.parentNode.parentNode);
+  }
+});
 
-    // カレンダーの見出しを設定
-    var title = year + "年" + (month) + "月";
-    calendar.innerHTML = "<h2>" + title + "</h2>";
+// Function to add a new row to the table
+function addRow() {
+  // Create new row and cells
+  const newRow = table.insertRow();
+  const nameCell = newRow.insertCell();
+  const actionCell = newRow.insertCell();
 
-    // カレンダーの表を生成
-    var table = "<table class=\"table table-bordered\"><thead><tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr></thead><tbody>";
+  // Add input and buttons to the new row
+  nameCell.innerHTML = '<input type="text" name="name">';
+  actionCell.innerHTML = '<button class="save-button">Save</button>';
 
-    // カレンダーの最初の週を生成
-    table += "<tr>";
-    for (var i = 0; i < firstDay.getDay(); i++) {
-        table += "<td></td>";
-    }
-    for (var i = 1; i <= 7 - firstDay.getDay(); i++) {
-        table += "<td>" + i + "</td>";
-    }
-    table += "</tr>";
-
-    // カレンダーの中間の週を生成
-    var day = 7 - firstDay.getDay() + 1;
-    while (day <= lastDay.getDate() - 7) {
-        table += "<tr>";
-        for (var i = 0; i < 7; i++) {
-            table += "<td>" + day + "</td>";
-            day++;
-        }
-        table += "</tr>";
-    }
-
-    // カレンダーの最後の週を生成
-    table += "<tr>";
-    console.log(day);
-    console.log(lastDay.getDay());
-    for (var i = day; i <= lastDay.getDate(); i++) {
-        table += "<td>" + i + "</td>";
-    }
-    for (var i = 1; i < 7 - lastDay.getDay(); i++) {
-        table += "<td></td>";
-    }
-    table += "</tr>";
-
-    table += "</tbody></table>";
-    calendar.innerHTML += table;
+  // Disable add row button
+  addButton.disabled = true;
 }
 
-createCalendar(2023, 4);
+// Function to edit an existing row
+function editRow(row) {
+  // Get cell and value of the selected row
+  const nameCell = row.cells[0];
+  const nameValue = nameCell.innerHTML;
+
+  // Replace cell content with input
+  nameCell.innerHTML = '<input type="text" name="name" value="' + nameValue + '">';
+
+  // Disable add row button
+  addButton.disabled = true;
+}
+
+// Function to delete an existing row
+function deleteRow(row) {
+  row.parentNode.removeChild(row);
+}
+
+// Add event listener for save buttons in the table
+table.addEventListener("click", function(event) {
+  if (event.target.classList.contains("save-button")) {
+    saveRow(event.target.parentNode.parentNode);
+  }
+});
+
+// Function to save an edited or newly added row
+function saveRow(row) {
+  // Get input and update cell content
+  const nameValue = row.cells[0].querySelector("input").value;
+  row.cells[0].innerHTML = nameValue;
+
+  // Enable add row button
+  addButton.disabled = false;
+}
